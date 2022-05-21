304b8147e4793f9b656210da00ee623a467a0665
Animesh Dey <animesh.dey986@gmail.com>
2022-05-22 01:13:02 +0530

    bug fixing

--- src/urlController.js
+++ src/urlController.js
@@ -6,22 +6,28 @@ const { promisify } = require("util");
 
 
 //Connect to redis
-const redisClient = redis.createClient(19382, "redis-19382.c301.ap-south-1-1.ec2.cloud.redislabs.com", { no_ready_check: true });
-redisClient.auth("DDebcA8K76cXEsz5jeMl2nri7ir0VrUl", function (err) {
+const redisClient = redis.createClient(
+    19382, 
+    "redis-19382.c301.ap-south-1-1.ec2.cloud.redislabs.com", 
+    { no_ready_check: true });
+
+    redisClient.auth("DDebcA8K76cXEsz5jeMl2nri7ir0VrUl", function (err) {
     if (err) throw err;
 });
 
 redisClient.on("connect", async function () {
     console.log("Redis is connected...");
 });
+redisClient.on("error", async function (err) {
+    console.log(err.message);
+});
+
 
 
 //Connection setup for redis
-const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
-// const SETEX_ASYNC = promisify(redisClient.SETEX).bind(redisClient)
-// const GETEX_ASYNC = promisify(redisClient.GETEX).bind(redisClient);
-// const DEFAULT_EXPIRATION = 24*60*60  //In seconds
+const SETEX_ASYNC = promisify(redisClient.SETEX).bind(redisClient)
+const DEFAULT_EXPIRATION = 60*60  //In seconds
 
 //---------------------------Valiadtions-----------------------------------------//
 //request body validation
@@ -37,28 +43,33 @@ const isValidValue = function (value) {
 //---------------------------------------------------Shorten Url API-----------------------------------------------------------------//
 const shortenUrl = async (req, res) => {
     try {
-        const longUrl = req.body.longUrl
+        if (!isValidRequest(req.body)) return res.status(400).send({ status: false, message: "No input by user" })
+        if (!isValidValue(req.body.longUrl)) return res.status(400).send({ status: false, message: "longUrl is required." })
+        
+        const longUrl = req.body.longUrl.trim()
 
         //If URL already Present in cache
         const cachedLongUrl = await GET_ASYNC(`${longUrl}`)
-        if (cachedLongUrl) {
+        if (cachedLongUrl) {                                      
             const parseLongUrl = JSON.parse(cachedLongUrl)
             return res.status(201).send({ status: true, message: "Shorten link already generated previously (from cache)", data: parseLongUrl })
         }
+
+         // If longurl present in db but not in cache
+         const usedLongUrl = await urlModel.findOne({ longUrl }).select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 })
+         if (usedLongUrl) return res.status(201).send({ status: true, message: "Shorten link already generated previously (from db).", data: usedLongUrl })
+
         //Url Validations
-        if (!isValidRequest(req.body)) return res.status(400).send({ status: false, message: "No input by user" })
-        if (!isValidValue(longUrl)) return res.status(400).send({ status: false, message: "longUrl is required." })
-        if (!validUrl.isWebUri(longUrl)) return res.status(400).send({ status: false, message: "Long Url is invalid." })
+        
+        const checkDot = longUrl.split('//')
+        if (!validUrl.isWebUri(longUrl) || !checkDot[1].includes('.') ) return res.status(400).send({ status: false, message: "Long Url is invalid." })
 
-        // If longurl present in db but not in cache
-        const usedLongUrl = await urlModel.findOne({ longUrl }).select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 })
-        if (usedLongUrl) return res.status(201).send({ status: true, message: "Shorten link already generated previously (from db).", data: usedLongUrl })
-
+        // Base Url
         const baseUrl = "http://localhost:3000/"
         if (!validUrl.isWebUri(baseUrl)) return res.status(400).send({ status: false, message: `${baseUrl} is invalid base Url` })
 
         //Short id generation
-        const shortUrlCode = shortid.generate()
+        const shortUrlCode = shortid.generate().toLowerCase()
         const alreadyExistCode = await urlModel.findOne({ urlCode: shortUrlCode })
         if (alreadyExistCode) return res.status(400).send({ status: false, message: `${alreadyExistCode} is already exist` })
 
@@ -68,7 +79,7 @@ const shortenUrl = async (req, res) => {
 
         //Set cache the newly created url
         if (generateUrl) {
-            await SET_ASYNC(`${longUrl}`, JSON.stringify(generateUrl))
+            await SETEX_ASYNC(`${longUrl}`, DEFAULT_EXPIRATION, JSON.stringify(generateUrl))
         }
         await urlModel.create(generateUrl)
         return res.status(201).send({ status: true, message: "Short url Successfully created", data: generateUrl })
@@ -91,7 +102,7 @@ const getUrl = async (req, res) => {
         const findUrlcode = await urlModel.findOne({ urlCode })
         if (!findUrlcode) return res.status(404).send({ status: false, message: "URL code not found" })
 
-        await SET_ASYNC(`${urlCode}`,JSON.stringify(findUrlcode))
+        await SETEX_ASYNC(`${urlCode}`,DEFAULT_EXPIRATION, JSON.stringify(findUrlcode))
         return res.status(302).redirect(findUrlcode.longUrl)
     } catch (err) {
         return res.status(500).send({ status: false, message: err.message })
