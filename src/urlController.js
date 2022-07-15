7963a45eb95eb069d3a8501ae4068575c5fe704d
Animesh Dey <animesh.dey986@gmail.com>
2022-07-16 01:38:25 +0530

    redis client url update

--- src/urlController.js
+++ src/urlController.js
@@ -4,14 +4,14 @@ const shortid = require('shortid')
 const redis = require('redis')
 const { promisify } = require("util");
 
-/*
+
 //Connect to redis
 const redisClient = redis.createClient(
-    19382, 
-    "redis-19382.c301.ap-south-1-1.ec2.cloud.redislabs.com", 
+    12507, 
+    "redis-12507.c212.ap-south-1-1.ec2.cloud.redislabs.com", 
     { no_ready_check: true });
 
-    redisClient.auth("DDebcA8K76cXEsz5jeMl2nri7ir0VrUl", function (err) {
+    redisClient.auth("9rnzcUHwyTiFHXdtehUSNNYFMfRGcCGJ", function (err) {
     if (err) throw err;
 });
 
@@ -21,14 +21,13 @@ redisClient.on("connect", async function () {
 redisClient.on("error", async function (err) {
     console.log(err.message);
 });
-
+//Api-key=> S1upb5sysvs8yb9tjhwht78rzapq009qk9ru64m8tcmtpf4jhch
 
 
 //Connection setup for redis
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 const SETEX_ASYNC = promisify(redisClient.SETEX).bind(redisClient)
 const DEFAULT_EXPIRATION = 60*60  //In seconds
-*/
 
 
 //---------------------------Valiadtions-----------------------------------------//
@@ -42,29 +41,30 @@ const isValidValue = function (value) {
     if (typeof value === 'string' && value.trim().length === 0) return false
     return true
 }
+
 //---------------------------------------------------Shorten Url API-----------------------------------------------------------------//
 const shortenUrl = async (req, res) => {
     try {
         if (!isValidRequest(req.body)) return res.status(400).send({ status: false, message: "No input by user" })
         if (!isValidValue(req.body.longUrl)) return res.status(400).send({ status: false, message: "longUrl is required." })
-        
+
         const longUrl = req.body.longUrl.trim()
 
         //If URL already Present in cache
         const cachedLongUrl = await GET_ASYNC(`${longUrl}`)
-        if (cachedLongUrl) {                                      
+        if (cachedLongUrl) {
             const parseLongUrl = JSON.parse(cachedLongUrl)
             return res.status(201).send({ status: true, message: "Shorten link already generated previously (from cache)", data: parseLongUrl })
         }
 
-         // If longurl present in db but not in cache
-         const usedLongUrl = await urlModel.findOne({ longUrl }).select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 })
-         if (usedLongUrl) return res.status(201).send({ status: true, message: "Shorten link already generated previously (from db).", data: usedLongUrl })
+        // If longurl present in db but not in cache
+        const usedLongUrl = await urlModel.findOne({ longUrl }).select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 })
+        if (usedLongUrl) return res.status(201).send({ status: true, message: "Shorten link already generated previously (from db).", data: usedLongUrl })
 
         //Url Validations
-        
+
         const checkDot = longUrl.split('//')
-        if (!validUrl.isWebUri(longUrl) || !checkDot[1].includes('.') ) return res.status(400).send({ status: false, message: "Long Url is invalid." })
+        if (!validUrl.isWebUri(longUrl) || !checkDot[1].includes('.')) return res.status(400).send({ status: false, message: "Long Url is invalid." })
 
         // Base Url
         const baseUrl = "http://localhost:3000/"
@@ -104,7 +104,7 @@ const getUrl = async (req, res) => {
         const findUrlcode = await urlModel.findOne({ urlCode })
         if (!findUrlcode) return res.status(404).send({ status: false, message: "URL code not found" })
 
-        await SETEX_ASYNC(`${urlCode}`,DEFAULT_EXPIRATION, JSON.stringify(findUrlcode))
+        await SETEX_ASYNC(`${urlCode}`, DEFAULT_EXPIRATION, JSON.stringify(findUrlcode))
         return res.status(302).redirect(findUrlcode.longUrl)
     } catch (err) {
         return res.status(500).send({ status: false, message: err.message })
