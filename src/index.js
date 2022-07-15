db27052f88239439eefd2f44739989df7c8de4a7
Animesh Dey <animesh.dey986@gmail.com>
2022-07-15 22:03:20 +0530

    body-parser removed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 const route = require('./routes');
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect("mongodb+srv://animesh-dey98:9I9JRLwql3bINqUX@cluster0.vhmqo.mongodb.net/project-urlShortener", {
     useNewUrlParser: true
